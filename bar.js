046100e6cbffee44df6a0b76b7241e5cf2c56077
Nikita <nick@Nikitas-MacBook-Pro.local>
2020-03-12 13:59:45 +0200

    show bar button, widget template

---
+++ bar.js
@@ -0,0 +1,11 @@
+function Bar() {
+    this.show = () => {
+        console.log('show bar');
+    };
+    this.hide = () => {
+        console.log('hide bar');
+    };
+    this.toggle = () => {
+        console.log('toggle bar');
+    };
+}
